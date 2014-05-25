c0eeaf78d60ae0ba51124ce0594586c91b5825a7
Ivan_Pl <Ivan_Pl@list.ru>
2014-05-25 19:44:55 +0700

    add Copyrights to default files

--- android/src/main/java/me/z_wave/android/MainActivity.java
+++ android/src/main/java/me/z_wave/android/MainActivity.java
@@ -1,16 +1,31 @@
+/*
+ * Z-Way for Android is a UI for Z-Way server
+ *
+ * Created by Ivan Platonov on 25.05.14 19:36.
+ * Copyright (c) 2014 Z-Wave.Me
+ *
+ * All rights reserved
+ * info@z-wave.me
+ * Z-Way for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Z-Way for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Z-Way for Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.z_wave.android;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
+import android.view.*;
 
 public class MainActivity extends Activity {
 
