bbdc7feda445cf30005c68a82d4f14a6e527f971
Ivan_Pl <Ivan_Pl@list.ru>
2014-05-26 21:47:50 +0700

    implement actionbar tab navigation

--- android/src/main/java/me/z_wave/android/MainActivity.java
+++ android/src/main/java/me/z_wave/android/MainActivity.java
@@ -22,43 +22,86 @@
 
 package me.z_wave.android;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.*;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ActionBar.TabListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        getActionBar().addTab(createTab(R.string.dashboard, R.drawable.ic_dashboard));
+        getActionBar().addTab(createTab(R.string.widgets, R.drawable.ic_widgets));
+        getActionBar().addTab(createTab(R.string.notifications, R.drawable.ic_notifications));
+        getActionBar().addTab(createTab(R.string.profiles, R.drawable.ic_profiles));
+        return super.onCreateOptionsMenu(menu);
     }
 
+    private ActionBar.Tab createTab(int titleId, int iconId){
+        final View tabView = createTabView(titleId, iconId);
+        return getActionBar().newTab().setCustomView(tabView)
+                .setTabListener(this);
+    }
+
+    /**
+     * Called when a tab enters the selected state.
+     * @param tab The tab that was selected
+     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
+     *            during a tab switch. The previous tab's unselect and this tab's select will be
+     *            executed in a single transaction. This FragmentTransaction does not support
+     */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new PlaceholderFragment())
+                    .commit();
+    }
+
+    /**
+     * Called when a tab exits the selected state.
+     * @param tab The tab that was unselected
+     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
+     *            during a tab switch. This tab's unselect and the newly selected tab's select
+     *            will be executed in a single transaction. This FragmentTransaction does not
+     */
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    /**
+     * Called when a tab that is already selected is chosen again by the user.
+     * Some applications may use this action to return to the top level of a category.
+     * @param tab The tab that was reselected.
+     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
+     *            once this method returns. This FragmentTransaction does not support
+     */
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 
     /**
@@ -75,5 +118,16 @@ public class MainActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    private View createTabView(int titleId, int iconId){
+        final LayoutInflater inflater = getLayoutInflater();
+        final View tabView = inflater.inflate(R.layout.layout_tab_view, null);
+        final TextView tabTitle = (TextView) tabView.findViewById(R.id.tab_title);
+        final ImageView tabIcon = (ImageView) tabView.findViewById(R.id.tab_icon);
+
+        tabIcon.setImageResource(iconId);
+        tabTitle.setText(titleId);
+        return tabView;
     }
 }
